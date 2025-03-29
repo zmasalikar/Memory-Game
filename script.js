0aebb96a3b20b20e9989d692c53ad1955be164f1
Zuber Masalikar <zuber.masalikar@hotmail.com>
2025-03-29 18:44:53 +0530

    updated

--- script.js
+++ script.js
@@ -1,58 +1,62 @@
 document.addEventListener("DOMContentLoaded", () => {
+    // -------------- Query Selection -------------
+    const resetbtn = document.getElementById("resetbtn");
+    const cards = Array.from(document.getElementsByClassName("card"));
+    
 
-// --------------query selection-------------
+    // -------------- Variables -------------
+    let newCards = [];
+    let move = 0;
+    let winCount = 0;
+    let movesDisplay = document.createElement("p");
+    document.body.appendChild(movesDisplay);
 
-const resetbtn = document.getElementById("resetbtn");
-const cards = document.getElementsByClassName("card");
+    // -------------- Card Flip -------------
+    cards.forEach(card => {
+        card.addEventListener("click", function () {
+            this.classList.add("flipped");
+            newCards.push(this); 
+            
+            if (newCards.length === 2) {
+                let lastCard = newCards[0].querySelector(".card-back").innerText;
+                let recentCard = newCards[1].querySelector(".card-back").innerText;
 
+                if (lastCard !== recentCard) {
+                    setTimeout(() => {
+                        newCards[0].classList.remove("flipped");
+                        newCards[1].classList.remove("flipped");
+                        newCards = [];
+                    }, 500);
+                } else {
+                    newCards = [];
+                    winCount++;
+                }
 
-//  --------------Reset-------------//
+                move++;
+                movesDisplay.innerText = `Moves: ${move}`;
 
-resetbtn.addEventListener("click", () => {
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].classList.remove("flipped"); 
-    }
-});
-
-//  --------------card flip-------------//
-
-    for (let i = 0; i < cards.length; i++) {
-        cards[i].addEventListener("click", () => {
-            cards[i].classList.toggle("flipped");
+                if (winCount === cards.length / 2) {
+                    alert(`You won the game in ${move} moves.`);
+                    
+                }
+            }
         });
-    }
+    });
 
-//  --------------card flip-------------//
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    // -------------- Reset -------------
+    resetbtn.addEventListener("click", () => {
+        cards.forEach(card => card.classList.remove("flipped")); 
+    
+        newCards = [];
+        move = 0;
+        winCount = 0;
+        movesDisplay.innerText = `Moves: ${move}`; 
+        
+        let cardsArray = [...cards];
+        cardsArray.sort(() => Math.random() - 0.5);
+    
+        let gameBoard = document.querySelector(".container"); 
+        gameBoard.innerHTML = ""; 
+        cardsArray.forEach(card => gameBoard.appendChild(card));
+    });
 });
-
